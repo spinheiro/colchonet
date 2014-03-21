916d0eedd71fde352fd5c315c11fa27863f19499
SamuelPinheiro <samuel.flf@gmail.com>
2014-03-21 15:44:09 -0300

    Commit Inicial

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def new
+    @user=User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def create
+    params.permit!
+    @user = User.new(params[:user])
+
+    if @user.save
+      redirect_to @user,
+                  :notice=> 'Cadastro criado com sucesso!'
+    else
+      render :new
+    end
+  end
+
+  def update
+    params.permit!
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(params[:user])
+      redirect_to @user,
+                  :notice=> 'Cadastro atualizado com sucesso!'
+    else
+      render :update
+    end
+  end
+
+end
